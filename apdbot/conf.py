2a51d9e0d18ae6daf0622af486019f6b915d4a5e
oxnet_ir <96020775+adoxnet@users.noreply.github.com>
2024-12-30 16:37:52 +0330

    Update conf.py

--- apdbot/conf.py
+++ apdbot/conf.py
@@ -3,7 +3,7 @@ from apdbot.sample_con import Config
 
 class Config(Config):
     BOT_TOKEN= "6690770296:AAF0bWnU9P5iwP4xzJ0yyuYpC1hIYFCjqmY"
-    APP_ID =14507494 
+    APP_ID = 14507494 
     API_HASH = "d6473f06d5643cef288db311c129db6c"
     OWNER_ID = "5748330516"
     AUTH = [-1002125882953, -100088888, -10002252254]
